9cb6c4f5b1d69448247644763b67dd5027043588
james <james9203@126.com>
2019-10-21 10:11:56 +0800

    variable

---
+++ variable/variable.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+extern int a,b;
+extern int c;
+extern float f;
+int main()
+{
+	int a,b;
+	int c;
+	float f;
+	
+	a=10;
+	b=20;
+	c=a+b;
+	cout << c << endl;
+	
+	f= 7.0/3.0;
+	cout << f << endl;
+	return 0;
+}

8d5efba15ab0a2b9248b61bc4c84b3e1a2e13887
james <james9203@126.com>
2019-11-02 16:10:37 +0800

    e_7

---
+++ example/e_7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char a=176,b=219;
+	cout<<b<<a<<a<<a<<b<<endl;
+	cout<<a<<b<<a<<b<<a<<endl;
+	cout<<a<<a<<b<<a<<a<<endl;
+	cout<<a<<b<<a<<b<<a<<endl;
+	cout<<b<<a<<a<<a<<b<<endl;
+	return 0;
+}
